0efef687bd73fcd212d3ae9f962415aa93f2a961
itoitto <arasi_rainbow@yahoo.co.jp>
2019-04-13 08:13:26 +0100

    検索機能実装

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,9 @@
 class Todo < ApplicationRecord
+  def self.search(search)
+    if search
+      Todo.where(['(content LIKE ?) or (name LIKE ?)', "%#{search}%", "%#{search}%"])
+    else
+      Todo.all
+    end
+  end
 end
