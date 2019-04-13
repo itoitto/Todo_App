ee6841cc6bb00862ddac1faee293ff98a22bf90f
itoitto <arasi_rainbow@yahoo.co.jp>
2019-04-13 07:11:24 +0100

    is_done 抽出条件追加

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,7 +4,7 @@ class TodosController < ApplicationController
   # GET /todos
   # GET /todos.json
   def index
-    @todos = Todo.all
+    @todos = Todo.all.where(is_done: 0)
   end
 
   # GET /todos/1
@@ -51,14 +51,21 @@ class TodosController < ApplicationController
     end
   end
 
-  # DELETE /todos/1
-  # DELETE /todos/1.json
+  # PATCH /todos/1
+  # PATCH /todos/1.json
   def destroy
     @todo.destroy
     respond_to do |format|
       format.html { redirect_to todos_url, notice: 'Todo was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  # PATCH /todos/1
+  # PATCH /todos/1.json
+  def done
+    @todo = Todo.find(params[:id])
+    @todo.toggle!(:is_done)
   end
 
   private
@@ -69,6 +76,6 @@ class TodosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def todo_params
-      params.require(:todo).permit(:name, :content, :end_date)
+      params.require(:todo).permit(:name, :content, :end_date, :is_done)
     end
 end
